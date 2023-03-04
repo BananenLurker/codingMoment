d16c3b36e3d49f78a20a6227d4c1e617ad0f6a2e
BananenLurker <daanvndputten@gmail.com>
2023-03-04 19:10:32 +0100

    Added infinite scroll

--- island/main.js
+++ island/main.js
@@ -72,3 +72,13 @@ function animate(){
 }
 
 animate();
+
+window.addEventListener("scroll", function() {
+  var scrollHeight = document.documentElement.scrollHeight;
+  var scrollTop = document.documentElement.scrollTop;
+  var clientHeight = document.documentElement.clientHeight;
+  
+  if (scrollTop + clientHeight >= scrollHeight) {
+    window.scrollTo(0, 0);
+  }
+});
