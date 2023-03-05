1c4608f88cc670f6241206b248b1625d584d0105
BananenLurker <daanvndputten@gmail.com>
2023-03-05 19:14:27 +0100

    added bg animation to mode switch

--- panningGallery/modes.js
+++ panningGallery/modes.js
@@ -4,6 +4,7 @@ const darkmodeButton = document.querySelector('#darkmode-button');
 
 darkmodeButton.addEventListener('click', () => {
     body.classList.toggle('darkmode');
+    body.classList.toggle('lightmode');
     gallery.classList.toggle('darkmode');
     darkmodeButton.classList.toggle('button-darkmode');
 })
