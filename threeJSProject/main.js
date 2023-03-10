2f5f53e046c8e26c0a00b4ef8e91f88b02347fb7
BananenLurker <daanvdp9@gmail.com>
2023-03-10 20:04:03 +0100

    removed unused code, changed some index formatting

--- threeJSProject/main.js
+++ threeJSProject/main.js
@@ -1,7 +1,5 @@
 import './style.css';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from './node_modules/.vite/deps/three';
 
 // Setup
 
