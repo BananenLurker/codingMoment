dc758e5ba68b1642189f5d2a78b775279e832089
BananenLurker <daanvdp9@gmail.com>
2023-09-13 16:49:47 +0200

    Werkcollege 11/09 - #103

--- CS_23_24/imperatiefProgrammeren/verKdag_Python/verKdag.py
+++ CS_23_24/imperatiefProgrammeren/verKdag_Python/verKdag.py
@@ -1,17 +1,19 @@
 from datetime import date, datetime, timedelta
 
-print("Wat is je geboortejaar?")
+print("Wat is je geboortejaar? ", end="")
 y = input()
-print("Hoeveelste maand?")
+print("Hoeveelste maand? ", end="")
 m = input()
-print("Welke dag?")
+print("Welke dag? ", end="")
 d = input()
+print("------------------------------------------------------------------------")
 
 geboorteDatum = datetime(int(y), int(m), int(d)).date()
 datumNu = date.today()
 
 verschil = datumNu - geboorteDatum
-vorigeVerKdag = datumNu + timedelta(days=-verschil.days % 1000)
+
+vorigeVerKdag = datumNu + timedelta(days=-(verschil.days % 1000))
 volgendeVerKdag = datumNu + timedelta(days=1000-verschil.days % 1000)
 volgendeKroonVerKDag = datumNu + timedelta(days=8000-verschil.days % 8000)
 
@@ -25,3 +27,4 @@ print(f"Je volgende kroon-verKdag is op {volgendeKroonVerKDag}.")
 
 if(volgendeVerKdag == volgendeKroonVerKDag):
     print("Je volgende verKdag is ook je kroon-verKdag!")
+print("------------------------------------------------------------------------")
---
+++ shinyHuntBot/clicker.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+
+time.sleep(2)
+n = 0
+for _ in range(10):
+    print('n')
+    time.sleep(n*2)
+    pyautogui.click
+    n=+1
