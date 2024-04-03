1a66ca689461657c86b959d4467843cf1da53085
BananenLurker <daanvdp9@gmail.com>
2024-04-03 15:42:00 +0200

    Added logging using morgan and rotating-file-stream to split logfiles up into a maximum of 1 day long sections

--- CS_23_24/webtech/HW3/app.js
+++ CS_23_24/webtech/HW3/app.js
@@ -4,8 +4,37 @@ const session = require('express-session');
 const path = require('path');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const morgan = require('morgan');
+const rfs = require('rotating-file-stream');
 
 const app = express();
+
+// Use padding to fill up day and month entries if they are single digits
+const logNamePadding = num => (num > 9 ? "" : "0") + num;
+// Construct the log name in the format YYYYMMDD-hhmm__[index]
+const logName = (time, index) => {
+  if (!time) return "file.log";
+
+  var year = time.getFullYear();
+  var month = logNamePadding(time.getMonth());
+  var day = logNamePadding(time.getDate());
+  var hour = logNamePadding(time.getHours());
+  var minute = logNamePadding(time.getMinutes());
+
+  return `${year}${month}${day}-${hour}${minute}__${index}-file.log`;
+};
+
+// For the log name: get the current date/time object and use the amount of stored logs as an index
+var accessLogStream = rfs.createStream(logName(new Date(), fs.readdirSync('./log').length), {
+  interval: '1d',
+  size: '100M',
+  path: path.join(__dirname, 'log')
+});
+
+// If res.statusCode => 400, an error has occurred and a dev log entry will be made
+// If there are no errors, logging will be done with less detail to save on storage space
+app.use(morgan('dev'), {skip: function(req, res) {return res.statusCode < 400}});
+app.use(morgan('common', { stream: accessLogStream }));
 
 app.use(session({
 	secret: 'secret',
@@ -15,64 +44,82 @@ app.use(session({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((request, response, next) => {
+app.use((req, res, next) => {
   // INSERT CODE FOR A PROFILE NAVBAR
-  console.log('Page loaded:', request.url);
+  console.log('Page loaded:', req.url);
   next();
 });
 
-app.get('/profile-template', function(request, response) {
-  response.redirect('/404');
+app.get('/log', function(req, res) {
+  console.log(index);
+})
+
+app.get('/profile-template', function(req, res) {
+  res.redirect('/404');
+})
+
+app.get('/profile-template.html', function(req, res) {
+  res.redirect('/404');
 })
 
 app.use(express.static(path.join(__dirname, 'static'), {
   extensions: ['html']
 }));
 
-app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/static/signup.html'));
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/static/signup.html'));
 });
 
-app.get('/login.css', (request, responseC) => {
+app.get('/login.css', (req, responseC) => {
   responseC.sendFile(path.join(__dirname, "/static/css/login.css"))
 });
 
-app.post('/signup', function(request, response) {
+app.post('/signup', function(req, res) {
   let db = openDatabase();
-  let userinfo = [request.body.username, request.body.password, request.body.email, request.body.country, request.body.city, request.body.zip];
+  let userinfo = [req.body.username, req.body.password, req.body.email, req.body.country, req.body.city, req.body.zip];
   let sql = 'INSERT INTO user(username, password, email, country, city, zip) VALUES(?,?,?,?,?,?)';
   const filePath = path.join(__dirname, 'static', 'signup.html');
 
-  checkExistance("username", request.body.username, function(usernameExists) {
+  checkExistance("username", req.body.username, function(usernameExists) {
     if (usernameExists) {
       closeDatabase(db);
-      userNotification(response, filePath, "Username already exists!");
+      userNotification(res, filePath, "Username already exists!");
       return;
     } 
     else {
-      checkExistance("email", request.body.email, function(emailExists) {
+      checkExistance("email", req.body.email, function(emailExists) {
         if (emailExists) {
           closeDatabase(db);
-          userNotification(response, filePath, "Email already exists!");
+          userNotification(res, filePath, "Email already exists!");
           return;
         } 
         else {
-          if(checkCharacters(request.body.username, request.body.email)){
+          if(checkCharacters(req.body.username, req.body.email)){
             closeDatabase(db);
-            userNotification(response, filePath, "Username or email contains excluded characters!");
+            userNotification(res, filePath, "Username or email contains excluded characters!");
             return;
           }
           else{
-            db.run(sql, userinfo, function(err) {
-              if (err) {
-                return console.log(err.message);
-              }
-              console.log(`A user has been created with ID ${this.lastID}.`);
-              request.session.username = request.body.username;
-              request.session.password = request.body.password;
+            if(hasUppercase(req.body.username)){
               closeDatabase(db);
-              response.redirect(302, '/profile');
-            });
+              userNotification(res, filePath, "Username cannot contain uppercase letters!");
+              return;
+            }
+            else{
+              db.run(sql, userinfo, function(err) {
+                if (err) {
+                  return console.log(err.message);
+                }
+                console.log(`A user has been created with ID ${this.lastID}.`);
+                req.session.username = req.body.username;
+                req.session.password = req.body.password;
+                req.session.email = req.body.email;
+                req.session.country = req.body.country;
+                req.session.zip = req.body.zip;
+                closeDatabase(db);
+                res.redirect(302, '/profile');
+              });
+            }
           }
         }
       });
@@ -80,11 +127,11 @@ app.post('/signup', function(request, response) {
   });
 });
 
-app.post('/auth', function(request, response) {
+app.post('/auth', function(req, res) {
   let db = openDatabase();
 
-  let user = request.body.username;
-  let passw = request.body.password;
+  let user = req.body.username;
+  let passw = req.body.password;
 
   if (user && passw) {
     let sql = "SELECT * FROM user WHERE username = ? AND password = ?";
@@ -94,75 +141,75 @@ app.post('/auth', function(request, response) {
     db.get(sql, [username, password], (err, row) => {
       if (err) {
         console.error(err.message);
-        response.status(500).send('Internal Server Error');
+        res.status(500).send('Internal Server Error');
         return;
       }
 
       if (row) {
-        request.session.loggedin = true;
-        request.session.username = username;
-        request.session.email = row.email;
-        request.session.country = row.country;
-        request.session.city = row.city;
-        request.session.zip = row.zip;
+        req.session.loggedin = true;
+        req.session.username = username;
+        req.session.email = row.email;
+        req.session.country = row.country;
+        req.session.city = row.city;
+        req.session.zip = row.zip;
 
-        response.redirect('/profile');
+        res.redirect('/profile');
       }
       else {
         console.log('login failed!');
-        response.redirect('/login');
+        res.redirect('/login');
       }
     });
   } 
   else {
     console.log('jup');
-    response.redirect('/login');
+    res.redirect('/login');
   }
 
   closeDatabase(db);
 });
 
-app.get('/profile', function(request, response) {
+app.get('/profile', function(req, res) {
   const filePath = path.join(__dirname, 'static', 'profile-template.html');
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       console.error('Error reading file:', err);
-      response.status(500).send('Internal Server Error');
+      res.status(500).send('Internal Server Error');
       return;
     }
 
     const $ = cheerio.load(data);
 
-    if(request.session.loggedin){
-      $('.profile-info__username').append(request.session.username);
-      $('.profile-info__email').append(request.session.email);
-      $('.profile-info__country').append(request.session.country);
-      $('.profile-info__city').append(request.session.city);
-      $('.profile-info__zip').append(request.session.zip);
+    if(req.session.loggedin){
+      $('.profile-info__username').append(req.session.username);
+      $('.profile-info__email').append(req.session.email);
+      $('.profile-info__country').append(req.session.country);
+      $('.profile-info__city').append(req.session.city);
+      $('.profile-info__zip').append(req.session.zip);
     }
     else{
       $('.temp').append('<p>Please log in <a href="login" class="link--simple">here</a>!</p>');
     }
 
-    response.send($.html());
+    res.send($.html());
   });
 });
 
-app.use((request, response) => {
-  response.status(404).sendFile(path.join(__dirname, 'static', '404.html'));
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'static', '404.html'));
 });
 
-function userNotification(response, filePath, errorType){
+function userNotification(res, filePath, errorType){
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       console.error('Error reading file:', err);
-      response.status(500).send('Internal Server Error');
+      res.status(500).send('Internal Server Error');
       return;
     }
 
     const $ = cheerio.load(data);
     $('.user-notification').append(errorType);
-    response.send($.html());
+    res.send($.html());
   })
 }
 
@@ -183,6 +230,10 @@ function closeDatabase(db){
     console.log('Closed the database connection.');
     console.log('--------------------------------');
   });
+}
+
+function hasUppercase(str){
+  return /^[A-Z]/.test(str);
 }
 
 function checkExistance(rowname, value, callback) {
@@ -213,6 +264,6 @@ function checkCharacters(username, email){
   }
 }
 
-app.listen(3000);
+app.listen(8001);
 
-console.log("The app is running on http://localhost:3000/");
+console.log("The app is running on http://localhost:8001/");
