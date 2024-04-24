864ced1dfacb5ee121de3da2a5e4cd66f8a1a9ee
BananenLurker <daanvdp9@gmail.com>
2024-04-24 14:43:20 +0200

    Added binary search dummy code

---
+++ CS_23_24/datastructuren/binary-search-dummy.cs
@@ -0,0 +1,21 @@
+// x is de vereiste grootte
+list<int> a = new list<int>;
+
+// Terminate if:
+// a[j] >= x;
+// a[j - 1] < x;
+
+int i = -1; j = int n; // Waarbij n een getal is dat in ieder geval groter is dan x
+while (!(i = j - 1))
+{
+    int m = (i + j) / 2 // Behoeft geen Math.Floor door integer afronding dor c#
+    if (a[m] < x)
+    {
+        i = m;
+    }
+    else
+    {
+        j = m;
+    }
+}
+return j;
